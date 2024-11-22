e87d77bef1dc9f83ea70b11112cabad1b0060fac
sammed <52048397+amazingsammed@users.noreply.github.com>
2024-11-22 21:52:13 +0000

    Revert "adding sam to main"

--- src/app/(home)/_component/mainbody.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Mainbody({ children,className }) {
-    return (
-        <div className={`max-w-screen-2xl  mx-auto px-10 ${className}`}>{children}</div>
-    );
-}
-
-export default Mainbody;
